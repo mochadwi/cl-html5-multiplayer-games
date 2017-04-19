5b89e7ab9cbfc7bb8684929212a0ea5cc771dd15
Mochamad Iqbal Dwi Cahyo <razeluxe20@gmail.com>
2017-04-19 18:38:56 +0700

    [Add] Integrating Socket.io with Express

--- app.js
+++ app.js
@@ -5,10 +5,15 @@ var
   UUID     = require('node-uuid'),
 
   verbose  = true,
-  app      = express();
+  app      = express(),
+  server   = require('http').Server(app),
+  // server = require('http').createServer(app),
+  sio      = io(server);
 
 // tell the game to listen for incoming connections
-app.listen(gameport);
+// app.listen(gameport);
+server.listen(gameport);
+
 // log to know if connected successfully
 console.log('\t :: Express :: Listening on port ' + gameport);
 // By default, redirect to this pages
@@ -30,3 +35,37 @@ app.get('/*', function(req, res, next)
   // send the requesting client the file.
   res.sendFile(__dirname + '/' + file);
 }); // app.get '/*'
+
+// Configure the socket.io connection settings.
+// sio.configure(function()
+// {
+//   sio.set('log level', 0);
+
+//   sio.set('authorization', function(handShake, callBack)
+//   {
+//     callBack(null, true); // error
+//   });
+// });
+
+// Socket.io call this function when client connects,
+// We can send that client a unique ID
+// so we can maintain the list of players
+sio.sockets.on('connection', function(client)
+{
+  // Generates new UUID
+  // and store this on client's socket / connection
+  client.userid = UUID;
+
+  // Tell the player they connected, and giving them their id
+  client.emit('onconnected', {id: client.userid});
+
+  // log when clients disconnects
+  console.log('\t socket.io :: Player ' + client.userid + ' connected!');
+
+  // When client disconnects
+  client.on('disconnect', function()
+  {
+    // log when clients disconnects
+    console.log('\t socket.io :: Player ' + client.userid + ' disconnected!');
+  }); // client.on disconnect
+}); // sio.sockets.on connection
